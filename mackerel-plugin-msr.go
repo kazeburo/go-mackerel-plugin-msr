086a9ff8b748b9230adc3e13f8b4d68bde62fec5
Masahiro Nagano <kazeburo@gmail.com>
2020-12-02 11:16:59 +0900

    support mysqlflags

--- mackerel-plugin-msr.go
+++ mackerel-plugin-msr.go
@@ -1,33 +1,35 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"log"
 	"os"
 	"runtime"
+	"strconv"
 	"time"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jessevdk/go-flags"
-	"github.com/ziutek/mymysql/mysql"
-	_ "github.com/ziutek/mymysql/native"
+	"github.com/kazeburo/go-mysqlflags"
 )
 
 // Version by Makefile
 var version string
 
-type connectionOpts struct {
-	Host    string `short:"H" long:"host" default:"localhost" description:"Hostname"`
-	Port    string `short:"p" long:"port" default:"3306" description:"Port"`
-	User    string `short:"u" long:"user" default:"root" description:"Username"`
-	Pass    string `short:"P" long:"password" default:"" description:"Password"`
-	Version bool   `short:"v" long:"version" description:"Show version"`
+type Opts struct {
+	mysqlflags.MyOpts
+	Timeout time.Duration `long:"timeout" default:"10s" description:"Timeout to connect mysql"`
+	Version bool          `short:"v" long:"version" description:"Show version"`
 }
 
 func main() {
 	os.Exit(_main())
 }
 
-func _main() (st int) {
-	opts := connectionOpts{}
+func _main() int {
+	opts := Opts{}
 	psr := flags.NewParser(&opts, flags.HelpFlag|flags.PassDoubleDash)
 	_, err := psr.Parse()
 	if opts.Version {
@@ -42,41 +44,98 @@ func _main() (st int) {
 		os.Exit(1)
 	}
 
-	db := mysql.New("tcp", "", fmt.Sprintf("%s:%s", opts.Host, opts.Port), opts.User, opts.Pass, "")
-	err = db.Connect()
+	now := time.Now().Unix()
+
+	db, err := mysqlflags.OpenDB(opts.MyOpts, opts.Timeout, false)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "couldn't connect DB\n")
-		return
+		log.Printf("couldn't connect DB: %v", err)
+		return 1
 	}
 	defer db.Close()
 
-	rows, _, err := db.Query("SELECT CHANNEL_NAME FROM performance_schema.replication_connection_status")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "couldn't execute query: %s\n", err)
-		return
-	}
+	ctx, cancel := context.WithTimeout(context.Background(), opts.Timeout)
+	defer cancel()
+	ch := make(chan error, 1)
 
-	if len(rows) == 0 {
-		fmt.Fprintf(os.Stderr, "no replication channels found\n")
-		return
-	}
+	behinds := map[string]int64{}
+	go func() {
+		rows, e := db.Query("SHOW SLAVE STATUS")
+		if e != nil {
+			ch <- e
+			return
+		}
+		defer rows.Close()
 
-	now := int32(time.Now().Unix())
-	for _, row := range rows {
-		channelName := row.Str(0)
-		query := fmt.Sprintf("SHOW SLAVE STATUS FOR CHANNEL '%s'\n", channelName)
-		slaveRows, slaveRes, err := db.Query(query)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "couldn't execute query")
+		cols, e := rows.Columns()
+		if e != nil {
+			ch <- e
+			return
+		}
+		vals := make([]interface{}, len(cols))
+		idxChannelName := -1
+		idxSecondsBehindMaster := -1
+		for i, v := range cols {
+			vals[i] = new(sql.RawBytes)
+			if v == "Channel_Name" {
+				idxChannelName = i
+			}
+			if v == "Seconds_Behind_Master" {
+				idxSecondsBehindMaster = i
+			}
+		}
+		if idxSecondsBehindMaster < 0 {
+			ch <- fmt.Errorf("Could not find Seconds_Behind_Master in columns")
 			return
 		}
 
-		idxSecondsBehindMaster := slaveRes.Map("Seconds_Behind_Master")
-		secondsBehindMaster := slaveRows[0].Int64(idxSecondsBehindMaster)
+		i := 0
+		for rows.Next() {
+			i++
+			e = rows.Scan(vals...)
+			if e != nil {
+				ch <- e
+				return
+			}
+			channelName := "-"
+			if idxChannelName >= 0 {
+				channelName = string(*vals[idxChannelName].(*sql.RawBytes))
+			}
+			strSecondsBehindMaster := string(*vals[idxSecondsBehindMaster].(*sql.RawBytes))
+			secondsBehindMaster, e := strconv.ParseInt(strSecondsBehindMaster, 10, 64)
+			if e != nil {
+				ch <- e
+				return
+			}
+			behinds[channelName] = secondsBehindMaster
+		}
+		if err := rows.Err(); err != nil {
+			ch <- err
+			return
+		}
 
-		fmt.Printf("mysql-msr.behind.%s\t%d\t%d\n", channelName, secondsBehindMaster, now)
+		if i == 0 {
+			ch <- fmt.Errorf("No replication settings")
+			return
+		}
+
+		ch <- nil
+	}()
+
+	select {
+	case err = <-ch:
+		// nothing
+	case <-ctx.Done():
+		err = fmt.Errorf("Connection or query timeout")
 	}
 
-	st = 0
-	return
+	if err != nil {
+		log.Printf("%v", err)
+		return 1
+	}
+
+	for n, behind := range behinds {
+		fmt.Printf("mysql-msr.behind.%s\t%d\t%d\n", n, behind, now)
+	}
+
+	return 0
 }
